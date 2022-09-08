03f953250fec44093d00ee099e1cc71be84bf660
Yinmu Liu <yinmu.liu@gmail.com>
2022-09-08 16:00:22 +1000

    Updates

---
+++ component---src-pages-imprint-js-3ef7fe8d324bd9f97c82.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_portfolio_minimal_theme=self.webpackChunkgatsby_starter_portfolio_minimal_theme||[]).push([[683],{3598:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var r=n(7294),i=n(6258);function l(){return r.createElement(r.Fragment,null,r.createElement(i.pQ,{title:"Imprint",useTitleTemplate:!0,noIndex:!0}),r.createElement(i.T3,null,r.createElement(i.Ek,{sectionId:"imprint",heading:"Imprint"})))}}}]);
+//# sourceMappingURL=component---src-pages-imprint-js-3ef7fe8d324bd9f97c82.js.map
