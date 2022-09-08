d0f96647d906864384c0dffb02bc794567d3ed37
Yinmu Liu <yinmu.liu@gmail.com>
2022-09-08 11:36:42 +1000

    fix in-page links, add resume

--- portfolio-minimal/src/pages/index.js
+++ portfolio-minimal/src/pages/index.js
@@ -16,11 +16,11 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
+        {/* <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} /> */}
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
         <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="contact" heading="Get in touch!" />
       </Page>
     </>
   );
